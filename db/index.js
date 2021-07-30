257f3823accf0a5fe94ab15e927af6440d1759ac
Daniel <danielorlandoa8@gmail.com>
2021-07-29 23:29:36 -0500

    changing db connection to url

--- db/index.js
+++ db/index.js
@@ -1,29 +1,17 @@
 const { Pool } = require('pg');
 const pool = new Pool({
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE,
+    connectionString: process.env.DATABASE_URL
 });
 
 pool.on('connect', () => {
-    console.log('Conexión establecida');
+    console.log('Connection done!');
 });
 
 
 module.exports = {
     async query(text, params) {
-        // invocation timestamp for the query method
-        const start = Date.now();
         try {
             const res = await pool.query(text, params);
-            // time elapsed since invocation to execution
-            const duration = Date.now() - start;
-            console.log(
-                'executed query',
-                { text, duration, rows: res.rowCount }
-            );
             return res;
         } catch (error) {
             console.log('error in query', { text });
